10e54bac6b04fb6229ed71389b6f6e436f625025
wuweizhuo <892073347@qq.com>
2025-09-11 11:58:25 +0800

    Update pra.c

--- pra.c
+++ pra.c
@@ -1,4 +1,6 @@
 # include<stdio.h>
+/*大数加法----体现了计算机，化繁为简以及循环遍历的应用*/
+
 # include<string.h>
 int main()
 {
@@ -6,8 +8,8 @@ int main()
     char num1[60],num2[60];
     printf("Enter numbers:");
     scanf("%s%s",num1,num2);
-    l1=strlen(num1),l2=strlen(num2);
-        for(int x=0;x<l1;x++)
+    l1=strlen(num1),l2=strlen(num2);                   //记录大数位数
+        for(int x=0;x<l1;x++)                          //存储各位数
             {
                 a[x]=num1[l1-1-x]-'0';
             }
@@ -24,13 +26,13 @@ int main()
                 }
                 else
                 {
-                    c[x]=(c[x]+a[x]+b[x])%10;
+                    c[x]=(c[x]+a[x]+b[x])%10;          //记得加自己，防止进位不全（易错）
                     c[x+1]=c[x+1]+(a[x]+b[x]+c[x])/10;
-                    if(x==l3-1) l3++;
+                    if(x==l3-1) l3++;                  //防止结果位数大于num1,numn2.
                 }
                 
             }
-    printf("The sum:");
+    printf("The sum:");                                //以字符形式输出结果
         for(int x=l3-1;x>=0;x--)
         {
             printf("%d",c[x]);
